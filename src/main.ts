fa5c0676446d2f9e83aeb967c1403ee1ed782ab6
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-09-04 20:57:16 +0700

    setup .env swagger prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const prefix = configService.get('PREFIX')
+
+  app.enableCors();
+  app.use(express.static("."))
+  app.setGlobalPrefix(prefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shoes shop')
+    .setDescription('The Shoes shop API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(port);
 }
 bootstrap();
