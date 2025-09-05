d45e676f56c99ee138300067c35d78c3b446a320
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-09-05 17:22:53 +0700

    modify structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
-import { JwtStrategy } from './modules/auth/strategy/jwt.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 import { SendMailModule } from './modules/send-mail/send-mail.module';
 import { EmailOtpModule } from './modules/email-otp/email-otp.module';
 import { UsersModule } from './modules/users/users.module';
